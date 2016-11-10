af22d4fe3da98f3f044a0d5246889c733d9306ea
BGrunenberg <b.grunenberg@gmx.de>
2016-11-10 10:51:45 +0100

    Websocket hinzugefügt

---
+++ Turnierplaner/Turnierplaner/Scripts/webSocketVerbindung.js
@@ -0,0 +1,27 @@
+﻿window.addEventListener("load", function () {
+var ws;
+
+try {
+    ws = new WebSocket("ws://localhost:49694/WebsocketHandler.ashx");
+
+    ws.onerror = function () {
+        console.log('Verbindung fehlgeschlagen');
+    };
+
+    ws.onopen = function () {
+        console.log("verbunden, readyState: " + this.readyState);
+    };
+
+    ws.onmessage = function (e) {
+
+    };
+
+    ws.onclose = function () {
+        console.log("Verbindung getrennt, readyState: " + this.readyState);
+    };
+}
+catch (e) {
+
+}
+
+});
---
+++ Turnierplaner/Turnierplaner/WebsocketHandler.ashx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace WebSocketServer
+{
+    /// <summary>
+    /// Summary description for WebSocketRequestHandler1
+    /// </summary>
+    public class WebSocketRequestHandler1 : IHttpHandler
+    {
+        private static List<WebSocket> clients = new List<WebSocket>();
+
+
+        public bool IsReusable
+        {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+                context.AcceptWebSocketRequest(Answer);
+        }
+
+        private async Task Answer(AspNetWebSocketContext con)
+        {
+            WebSocket socket = con.WebSocket;
+            clients.Add(socket);
+
+            while (socket.State == WebSocketState.Open)
+            {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+                var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+
+                foreach (WebSocket client in clients)
+                {
+                    if (!client.Equals(socket))
+                        await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+
+            clients.Remove(socket);
+        }
+    }
+}
